14ebb4fdb299adaeeadef2dc69827f15849fdee5
Roman <42904990+anufrievroman@users.noreply.github.com>
2021-07-13 12:22:16 +0900

    Sensible default file

--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
 # GENERAL PARAMETERS
-output_folder_name           = 'test'
-number_of_phonons            = 10
-number_of_phonons_in_a_group = 10
+output_folder_name           = 'Test'
+number_of_phonons            = 500
+number_of_phonons_in_a_group = 50
 number_of_timesteps          = 5000
 number_of_nodes              = 400
 timestep                     = 2.0e-12
@@ -16,29 +16,29 @@ simulation_mode              = 1
 # INTERNAL SCATTERING
 internal_scattering_on       = True
 use_gray_approximation_mfp   = False
-gray_approximation_mfp       = 5000e-9
+gray_approximation_mfp       = 500e-9
 
 # SYSTEM DIMENSIONS [m]
 width                        = 1200e-9
-length                       = 2100e-9
-thickness                    = 145e-9
+length                       = 1500e-9
+thickness                    = 150e-9
 
 # ROUGHNESS [m]
 side_wall_roughness          = 2.0e-9
-hole_roughness               = 40.0e-9
+hole_roughness               = 2.0e-9
 pillar_roughness             = 2.0e-9
 top_roughness                = 0.2e-9
 bottom_roughness             = 0.2e-9
 pillar_top_roughness         = 2.0e-9
 
 # HOLES AND PILLARS PARAMETERS [m]
-holes                        = 'no'
+holes                        = 'yes'
 hole_lattice_type            = 'square'
 pillars                      = 'no'
 pillar_lattice_type          = 'square'
-circular_hole_diameter       = 250e-9
-rectangular_hole_side_x      = 250e-9
-rectangular_hole_side_y      = 250e-9
+circular_hole_diameter       = 150e-9
+rectangular_hole_side_x      = 800e-9
+rectangular_hole_side_y      = 1020e-9
 pillar_height                = 30e-9
 from math import pi
 pillar_wall_angle            = pi/2.0
@@ -51,5 +51,5 @@ number_of_pixels_y           = 300
 number_of_timeframes         = 10
 
 # MATERIAL PARAMETERS
-specific_heat_capacity       = 714            # [J/kg/K] It's  0.0176 at 4K
-material_density             = 2330	                # [kg/m^3]
+specific_heat_capacity       = 714          # [J/kg/K] It's  0.0176 at 4K
+material_density             = 2330	    # [kg/m^3]
