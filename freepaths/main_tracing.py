0c4ecf7b800c43e435b8c4bc193d791197d25bea
Philipp G <30627896+phigas@users.noreply.github.com>
2023-12-07 15:18:29 +0900

    added multiprocessing to main mode

--- freepaths/main_tracing.py
+++ freepaths/main_tracing.py
@@ -5,6 +5,7 @@ import sys
 import time
 import shutil
 import colorama
+import multiprocessing
 from colorama import Fore, Style
 
 # Modules:
@@ -21,41 +22,124 @@ from freepaths.animation import create_animation
 from freepaths.output_plots import plot_data
 
 
+class PhononSimulator:
+    def __init__(self, worker_id, total_phonons, result_queue, output_trajectories_of):
+        self.worker_id = worker_id
+        self.total_phonons = total_phonons
+        self.result_queue = result_queue
+        self.creation_time = time.time()
+        self.output_trajectories_of = output_trajectories_of
+        
+        # Initiate data structures:
+        self.material = Material(cf.media, num_points=cf.number_of_phonons+1)   # ok
+        self.scatter_stats = ScatteringData()                                   # ok
+        self.general_stats = GeneralData()                                      # ok
+        self.segment_stats = SegmentData()                                      # ok
+        self.path_stats = PathData()                                            # ok
+        self.scatter_maps = ScatteringMap()                                     # ok
+        self.thermal_maps = ThermalMaps()                                       # ok
+        
+        self.total_thermal_conductivity = 0.0
+    
+    def simulate_phonons(self, render_progress=False):
+        
+        if render_progress:
+            progress = Progress()
+        
+        for index in range(self.total_phonons):
+            if render_progress:
+                progress.render(index, self.total_phonons)
+            
+            # Initiate a phonon and its flight:
+            phonon = Phonon(self.material)
+            flight = Flight(phonon)
+            
+            # Run this phonon through the structure:
+            run_phonon(phonon, flight, self.scatter_stats, self.segment_stats, self.thermal_maps, self.scatter_maps, self.material)
+
+            # Record the properties returned for this phonon:
+            self.general_stats.save_phonon_data(phonon)
+            self.general_stats.save_flight_data(flight)
+            
+            # Record trajectories of the first N phonons:
+            if index < self.output_trajectories_of:
+                self.path_stats.save_phonon_path(flight)
+        
+        # collect relevant data
+        collected_data = {
+            'scatter_stats': self.scatter_stats.dump_data(),
+            'general_stats': self.general_stats.dump_data(),
+            'segment_stats': self.segment_stats.dump_data(),
+            'path_stats': self.path_stats.dump_data(),
+            'scatter_maps': self.scatter_maps.dump_data(),
+            'thermal_maps': self.thermal_maps.dump_data(),
+            'execution_time': time.time() - self.creation_time,
+        }
+        
+        # put data into queue
+        self.result_queue.put(collected_data)
+
+
+def worker_process(worker_id, total_phonons, result_queue, output_trajectories_of):
+    simulator = PhononSimulator(worker_id, worker_id, total_phonons, result_queue, output_trajectories_of)
+    simulator.simulate_phonons(render_progress=1 if worker_id == 0 else 0)
+
+
 def main(input_file):
     """This is the main function, which works under Debye approximation.
     It should be used to simulate phonon paths at low temperatures"""
 
     print(f'Simulation of {Fore.GREEN}{cf.output_folder_name}{Style.RESET_ALL}')
     start_time = time.time()
-    progress = Progress()
+    
+    # create queue for collection of data
+    result_queue = multiprocessing.Queue()
+    
+    # Divide workload among workers
+    workload_per_worker = cf.number_of_phonons // cf.num_workers
+    remaining_phonons = cf.number_of_phonons % cf.num_workers
 
+    output_trajectories_per_worker = cf.output_trajectories_of_first // cf.num_workers
+    remaining_output_trajectories = cf.output_trajectories_of_first % cf.num_workers
+
+    # Create and start worker processes
+    processes = []
+    for i in range(cf.num_workers):
+        worker_phonons = workload_per_worker + (1 if i < remaining_phonons else 0)
+        output_trajectory_of = output_trajectories_per_worker + (1 if i < remaining_output_trajectories else 0)
+        process = multiprocessing.Process(target=worker_process, args=(i, worker_phonons, result_queue, output_trajectory_of))
+        processes.append(process)
+        process.start()
+    
+    # Wait for all processes to finish
+    for process in processes:
+        process.join()
+    
+    # Collect results from the queue
+    all_results = []
+    while not result_queue.empty():
+        result_data = result_queue.get()
+        all_results.append(result_data)
+    
     # Initiate data structures:
-    material = Material(cf.media)
+    # material = Material(cf.media, num_points=cf.number_of_phonons+1)
     scatter_stats = ScatteringData()
     general_stats = GeneralData()
     segment_stats = SegmentData()
     path_stats = PathData()
     scatter_maps = ScatteringMap()
     thermal_maps = ThermalMaps()
-
-    # For each phonon
-    for index in range(cf.number_of_phonons):
-        progress.render(index, cf.number_of_phonons)
-
-        # Initiate a phonon and its flight:
-        phonon = Phonon(material)
-        flight = Flight(phonon)
-
-        # Run this phonon through the structure:
-        run_phonon(phonon, flight, scatter_stats, segment_stats, thermal_maps, scatter_maps, material)
-
-        # Record the properties returned for this phonon:
-        general_stats.save_phonon_data(phonon)
-        general_stats.save_flight_data(flight)
-
-        # Record trajectories of the first N phonons:
-        if index < cf.output_trajectories_of_first:
-            path_stats.save_phonon_path(flight)
+    
+    # collect the results
+    while not result_queue.empty():
+        collected_data = result_queue.get()
+        scatter_stats.read_data(collected_data['scatter_stats'])
+        general_stats.read_data(collected_data['general_stats'])
+        segment_stats.read_data(collected_data['segment_stats'])
+        path_stats.read_data(collected_data['path_stats'])
+        scatter_maps.read_data(collected_data['scatter_maps'])
+        thermal_maps.read_data(collected_data['thermal_maps'])
+        print(collected_data['execution_time'])
 
     # Run additional calculations:
     thermal_maps.calculate_thermal_conductivity()
